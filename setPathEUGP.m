6ce47973bc5a632c6c599e832448eddf35259368
Edwin Bonilla <edwinbonilla@gmail.com>
2015-08-14 17:45:33 +1000

    path

---
+++ setPathEUGP.m
@@ -0,0 +1,11 @@
+% sets path for inference algorithms, mainly variational stuff
+
+%% This is for tunning Trung's code, order matters here
+addpath(genpath('code/inference/variational/mteugp'));
+addpath(genpath('code/inference/features'));
+
+
+
+%addpath(genpath('/Users/edwinbonilla/Dropbox/Matlab/minFunc_2012'));
+%addpath(genpath('/Users/edwinbonilla/Software/nlopt')); %'nlopt'
+
