3082fe9c21efa02088ae4f0820cb4de6927e5982
Edwin Bonilla <edwinbonilla@gmail.com>
2015-09-03 17:22:07 +1000

    minor on ugp

--- code/inference/variational/mteugp/mteugpUpdateLinearization.m
+++ code/inference/variational/mteugp/mteugpUpdateLinearization.m
@@ -9,19 +9,42 @@ Q = model.Q;
 A = zeros(N,P,Q);
 B = zeros(N,P);
 
-F = model.Phi*model.M;
+MuF = model.Phi*model.M;
 
 switch (model.linearMethod)
     case 'Taylor',
         for n = 1 : N
-            f = F(n,:);
+            f = MuF(n,:);
             [fval, Jn] = egpEvalFwdModel(model.fwdFunc, f);
             A(n,:,:) = Jn;
             B(n,:)   = fval - f*Jn'; 
         end
+    case 'Unscented'
+    VarF = getVariancesF(Phi, C);  % NxQ matrix of variances
+    for n = 1 : N
+        [A(n,:,:), B(n,:) ] = ugpGetLinearization(MuF(n,:), VarF(n,:), model.kappa);          
+    end
+        
     otherwise 
         fprintf('Unknown Linearization method');
 end
 
 
 end
+
+
+% get the variances var(fnq), q=1, Q
+function V = getVariancesF(Phi, C)
+% Phi: NxD matrix of features
+% C: DxDxQ matrix of Q DxD covariances (on weights)
+% V: NxQ vector of variances
+N = size(Phi,1);
+Q = size(C,3);
+V = zeros(N,Q);
+Phi2 = Phi.^2;
+for q = 1 : Q
+    cq = diag(C(:,:,q)); 
+    V(:,q) = Phi2*cq;
+end
+end
+
---
+++ code/inference/variational/mteugp/ugpGetLinearization.m
@@ -0,0 +1,13 @@
+function [ An, bn ] = ugpGetLinearization(fwdFunc, muF, varF, kappa)
+%UGPGETLINEARIZATION Summary of this function goes here
+%   Get linearization using the unscented transform
+% MuF: 1xQ vector of means 
+% VarF: 1xQ vector of variances
+% kappa: Parameter of the unscented transform 
+
+% We generate N = 2*Q + 1 sigma points, their weights and observations
+
+
+
+end
+
