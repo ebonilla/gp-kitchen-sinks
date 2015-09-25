a694c4cee8619ed39670cc0fb7f94686f2e5fff5
Edwin Bonilla <edwinbonilla@gmail.com>
2015-09-25 11:03:37 +1000

    path

--- setPathEUGP.m
+++ setPathEUGP.m
@@ -7,11 +7,11 @@ addpath(genpath('code/inference/features'));
 addpath('data');
 
 
-path(path(), genpath('~/Dropbox/Matlab/utils')); % sq_dist from here, plots, etc
-path(path(), genpath('~/Dropbox/Matlab/gpml-matlab-v3.6-2015-07-07')); % covfuncs
-path(path(), genpath('~/Dropbox/Matlab/DERIVESTsuite')); % jacobians
 
-addpath(genpath('~/Dropbox/Matlab/minFunc_2012'));
-
-addpath(genpath('/Users/ebonilla/Documents/software/nlopt')); %'nlopt'
-
+%path(path(), genpath('~/Dropbox/Matlab/utils')); % sq_dist from here, plots, etc
+%path(path(), genpath('~/Dropbox/Matlab/gpml-matlab-v3.6-2015-07-07')); % covfuncs
+%path(path(), genpath('~/Dropbox/Matlab/DERIVESTsuite')); % jacobians
+%addpath(genpath('~/Dropbox/Matlab/minFunc_2012'));
+%addpath(genpath('/Users/ebonilla/Documents/software/nlopt')); %'nlopt'
+% All these paths are now in external dir
+addpath(genpath('external'));
