3dc22a44b3d96af82d9677e87cbad21de64e17f1
Edwin Bonilla Pabon <z3503119@leonardi.eng.unsw.edu.au>
2015-09-28 13:40:36 +1000

    minor path things

--- run_mteugp_cluster.sh
+++ run_mteugp_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.bashrc
 
-/home/z3503119/research/projects/bayes-inv-var/research//standalone/toyData/run_mteugpToyData.sh  /home/z3503119/research/projects/bayes-inv-var/standalone/standalone/MCR/v84 $1 $2 $3 $4 $5
+/home/z3503119/research/projects/bayes-inv-var/standalone/toyData/run_mteugpToyData.sh  /home/z3503119/research/projects/bayes-inv-var/standalone/MCR/v84 $1 $2 $3 $4 $5
 
 
 
