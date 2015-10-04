7f480eb3816e1d02c730c6f4b52a37dca90bb3dc
Edwin Bonilla Pabon <z3503119@leonardi.eng.unsw.edu.au>
2015-10-04 13:17:30 +1100

    cluster scripts for usps binary

---
+++ submit_all_cluster_array_uspsBinary.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#PBS -q short48
+#PBS -N toyDataArray
+#PBS -l walltime=24:00:00 -l nodes=1:ppn=12,mem=8GB,vmem=8GB
+#PBS -J 1-6
+
+# Use: qsub submit_all_cluster_array_uspsBinary.sh
+
+##### Obtain Parameters from input.txt file using $PBS_ARRAY_INDEX as the line number #####
+parameters=`sed -n "${PBS_ARRAY_INDEX} p" input_usps_binary.txt`
+parameterArray=($parameters)
+idxMethod=${parameterArray[0]}
+d=${parameterArray[1]}
+boolSample=${parameterArray[2]}
+writeLog=${parameterArray[3]}
+
+##### Execute Program #####
+printf "Values used are %d %d %d %d\n" $idxMethod $d $boolSample $writeLog
+./run_mteugp_cluster_array_uspsBinary.sh $idxMethod $d $boolSample $writeLog
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
