d3ab74c0239f208c9107e1493cebab64caf73dd6
Edwin Bonilla <edwinbonilla@gmail.com>
2015-10-07 01:19:26 +1100

    matrix form of getNelboHyper

--- code/inference/variational/mteugp/mteugpInitFromFile.m
+++ code/inference/variational/mteugp/mteugpInitFromFile.m
@@ -5,16 +5,28 @@ function [model, exitCode]  = mteugpInitFromFile( model )
 exitCode = 0;
 try
     m = load(model.resultsFname);
-    fprintf('loading previous model from %s\n', model.resultsFname);
     % we copy one by one for backward compatibilty 
-    fNames = fieldnames(m.model);
-    for i = 1 : length(fNames)
-        % do not copy optimization / configuration fields
-        if ( isempty(strfind(fNames{i}, 'Conf')) && ...
-            isempty(strfind(fNames{i}, 'Func')) ) 
-            model.(fNames{i}) = m.model.(fNames{i});
-        end
-    end
+%    fNames = fieldnames(m.model);
+%     for i = 1 : length(fNames)
+%         % do not copy optimization / configuration fields
+%         if ( isempty(strfind(fNames{i}, 'Conf')) && ...
+%             isempty(strfind(fNames{i}, 'Func')) ) 
+%             model.(fNames{i}) = m.model.(fNames{i});
+%         end
+%     end
+    model.Z         =  m.model.Z;
+    model.sigma2y   =  m.model.sigma2y;   % likelihood variances
+    model.sigma2w   =  m.model.sigma2w;  % hyper-parameters (of prior on w)
+    model.M         =  m.model.M;
+    model.featParam =  m.model.featParam;
+    model.C         =  m.model.C;
+    model.Phi       = feval(model.featFunc, model.X, model.Z, model.featParam); 
+    model.D         = size(model.Phi,2); % actual number of features    
+    [model.A, model.B] = mteugpUpdateLinearization(model); % lineariz. parameters
+    model              = mteugpOptimizeCovariances( model );    
+    model.nelbo     =  m.model.nelbo;
+
+    fprintf('Parameters loaded from previous model at %s\n', model.resultsFname);
     exitCode = 1;
     return;
 catch ME
--- code/inference/variational/mteugp/mteugpNelboHyper.m
+++ code/inference/variational/mteugp/mteugpNelboHyper.m
@@ -20,8 +20,47 @@ nelbo =  getNelboHyper(model);
 end
 
 
+
+function nelbo =  getNelboHyper(model)
+sigma2y      = model.sigma2y;
+diagSigmaInv = 1./(sigma2y);
+P            = model.P;
+N            = model.N;
+Q            = model.Q;
+D            = model.D;
+
+PhiM    = model.Phi*model.M; % N*Q
+%[N P Q] = siz(model.A);
+C = zeros(N, P);
+for p = 1 : P
+    Ap     = squeeze(model.A(:,p,:));
+    C(:,p) = sum(Ap.*PhiM,2);
+end
+C        = model.Y - C - model.B;
+elbo = sum(sum(bsxfun(@times,C, diagSigmaInv).*C)); % quad term
+
+% remaining terms coming from KL and likelihood after cancelation of traces at optimal M,C
+elbo = elbo +  N*( P*log(2*pi) + sum(log(sigma2y)) );
+for q = 1 : Q
+    sigma2w = model.sigma2w(q);
+    C       = model.C(:,:,q); % posterior covariance
+    cholC   = getCholSafe(C);
+    m       = model.M(:,q); % posterior mean
+    elbo   = elbo +  (1/sigma2w)*(m'*m)  ...
+                  - getLogDetChol(cholC) ...
+                  + D*log(sigma2w);
+end
+
+elbo  = -0.5*elbo;
+
+nelbo = -elbo;
+
+
+end
+
+
 %% getNelboHyper
-function nelbo = getNelboHyper(model)
+function nelbo = getNelboHyperOld(model)
 % It actually uses a simplified version of the nelbo
 
 sigma2y     = model.sigma2y;
