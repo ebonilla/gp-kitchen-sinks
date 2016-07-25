f56ef93ed81bfc0bbf4c24da040d3268311fb02a
Edwin Bonilla <edwinbonilla@gmail.com>
2016-07-25 12:28:17 +1000

    rearranging code

--- setPathEUGP.m
+++ setPathEUGP.m
@@ -1,17 +1,7 @@
 % sets path for inference algorithms, mainly variational stuff
+% Edwin V. Bonilla (edwinbonilla@gmail.com)
 
-%% order may matter here
-addpath(genpath('code/inference/variational/mteugp'));
-addpath(genpath('code/inference/variational/utils'));
-addpath(genpath('code/inference/features'));
+addpath(genpath('src'));
+addpath(genpath('lib'));
 addpath('data');
 
-
-
-%path(path(), genpath('~/Dropbox/Matlab/utils')); % sq_dist from here, plots, etc
-%path(path(), genpath('~/Dropbox/Matlab/gpml-matlab-v3.6-2015-07-07')); % covfuncs
-%path(path(), genpath('~/Dropbox/Matlab/DERIVESTsuite')); % jacobians
-%addpath(genpath('~/Dropbox/Matlab/minFunc_2012'));
-%addpath(genpath('/Users/ebonilla/Documents/software/nlopt')); %'nlopt'
-% All these paths are now in external dir
-addpath(genpath('external'));
